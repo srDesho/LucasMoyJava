50398219f2ae12a9b7b26a92f1bd82a100efeaeb
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-02-08 16:42:11 -0400

    Arrays Practice, ManagementClients System, new Git Codes

--- ManagementSystem/src/Form.java
+++ ManagementSystem/src/Form.java
@@ -1,20 +1,43 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Form {
     public JPanel panel;
-    public JButton btnResult;
-    public JTextField txtName;
-
+    private JButton btnSave;
+    private JTextField txtName;
+    private JList listClients;
+    private JButton deleteButton;
+    private List<String> nameList = new ArrayList<String>();
 
     public Form() {
-        btnResult.addActionListener(new ActionListener() {
+        btnSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = txtName.getText();
-                JOptionPane.showMessageDialog(null, "Welcome " + name);
+                nameList.add(name);
+                refreshList();
+                JOptionPane.showMessageDialog(null, "Added success. " );
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = listClients.getSelectedIndex();
+                nameList.remove(index);
+                refreshList();
             }
         });
     }
+    public void refreshList() {
+        DefaultListModel data = new DefaultListModel();
+        for (int i = 0; i < nameList.size(); i++) {
+            String name = nameList.get(i);
+            data.addElement(name);
+        }
+        listClients.setModel(data);
+    }
+
 }
